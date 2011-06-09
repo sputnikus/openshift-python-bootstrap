e469ea3e32dc408c7b50073f33530fc3df3e0e8b
Martin Putniorz <mputniorz@gmail.com>
2011-06-09 22:35:54 +0200

    Add django support, tune inner logic

--- README.markdown
+++ README.markdown
@@ -2,7 +2,7 @@
 
 ## Why
 
-There's no official documentation from Red Hat about how to write your WSGI application for [OpenShift](https://openshift.redhat.com/app/express "OpenShift Express"). So I decided to create bootstrap script to prepare your application directory (currently [Flask](http://flask.pocoo.org/ "Flask microframework") only) for deployment.
+There's no official documentation from Red Hat about how to write your WSGI application for [OpenShift](https://openshift.redhat.com/app/express "OpenShift Express"). So I decided to create bootstrap script to prepare your repository for deployment of your [Django](https://www.djangoproject.com/ "Django web framework") or [Flask](http://flask.pocoo.org/ "Flask microframework") application.
 
 ## Usage
 
@@ -10,7 +10,7 @@ Create your app and virtual enviroment
 
      rhc-create-app -a <app name>
      cd <bootstrap directory>
-     ./bootstrap.py <app directory>/env
+     ./bootstrap.py -f <framework> -n <app name> <app directory>/env
 
 If you want to activate virtualenv:
      
@@ -19,5 +19,18 @@ If you want to activate virtualenv:
 
 And if you want to test your app locally
 
+**Flask**:
+     
      cd <app directory>/wsgi
      ./application
+
+**Django**:
+
+     cd <app directory>/libs/<app name>
+     python manage.py runserver<center></center><center></center> 
+
+## Thanks
+
+* [Ian Weller](http://blog.ianweller.org/2011/05/12/openshift-express-first-thoughts/ "Django in OpenShift")
+* [holly's wiki](http://wiki.livedoor.jp/kurt0027/lite/d/openshift%A4%CD%A4%BF "Flask in OpenShift")
+* [The virtualenv developers](https://github.com/pypa/virtualenv "Python virtualenv")
--- bootstrap.py
+++ bootstrap.py
@@ -3,6 +3,7 @@
 
 """
 Script prepares python enviroment for deploying into OpenShift cloud.
+Heavily inspired by virtualenv code (actually using some parts of it).
 For more see README
 """
 
@@ -10,6 +11,7 @@ import subprocess
 import os
 import sys
 import stat
+import optparse
 try:
     import virtualenv
 except ImportError:
@@ -17,18 +19,42 @@ except ImportError:
     print "Try pip install virtualenv or easy_install virtualenv"
     exit()
 
-def after_install(options, home_dir):
+def after_install(home_dir, options):
     """
     After install steps - copying files and creating application
     script
     """
+
+    if options.app_name == None:
+        app_name = "application"
+    else:
+        app_name = options.app_name
+
+    # you need Python2.6 package folder (for server side)
     if sys.version_info[1] == 7:
         python27 = os.path.join('.', 'python2.7')
         python26 = os.path.join(home_dir, 'lib', 'python2.6')
         os.symlink(python27, python26)
+
     pip = os.path.join(home_dir, 'bin', 'pip')
+
+    if options.framework.lower() == "flask":
+        install_flask(home_dir, app_name, pip)
+    elif options.framework.lower() == "django":
+        install_django(home_dir, app_name, pip)
+
+    gitignore = os.path.join(home_dir, '..', '.gitignore')
+    file(gitignore, 'w').write("""\
+*.pyc
+*.org
+*.bak
+*.old
+*.sw[po]
+""")
+
+def install_flask(home_dir, app_name, pip):
     subprocess.call([pip, 'install', 'flask'])
-    app_dir = os.path.join(home_dir, '..', 'libs', 'application')
+    app_dir = os.path.join(home_dir, '..', 'libs', app_name)
     os.makedirs(app_dir)
     # basic Flask application
     init = os.path.join(app_dir, '__init__.py')
@@ -66,7 +92,7 @@ def index():
 Hi, some instructions for you:
 <ul>
     <li>Your app goes to <code>&lt;project home&gt;/libs/application</code></li>
-    <li>I don't recommend changing <code>&lt;project home&gt;/wsgi/application</code></li>
+    <li>I don't recommend to change <code>&lt;project home&gt;/wsgi/application</code></li>
 {% endblock %}
 """)
     # and, of course, wsgi script
@@ -87,7 +113,7 @@ sys.path.append(flaskapp)
 execfile(activate, dict(__file__=activate))
 os.environ["PYTHON_EGG_CACHE"] = pythoneggs
 
-from application import app as application
+from """+app_name+""" import app as application
 
 sys.stdout = sys.stderr
 
@@ -97,26 +123,82 @@ if __name__ == '__main__':
     application.run()
 """)
     os.chmod(application, stat.S_IRWXU|stat.S_IRGRP|stat.S_IROTH)
-    gitignore = os.path.join(home_dir, '..', '.gitignore')
-    file(gitignore, 'w').write("""\
-*.pyc
-*.org
-*.bak
-*.old
-*.sw[po]
+
+def install_django(home_dir, app_name, pip):
+    subprocess.call([pip, 'install', 'django'])
+    libs = os.path.join(home_dir, '..', 'libs')
+    root = os.getcwd()
+    os.chdir(libs)
+    admin = os.path.join('..', home_dir, 'bin', 'django-admin.py')
+    subprocess.call([admin, 'startproject', app_name])
+    os.chdir(root)
+    application = os.path.join(home_dir, '..', 'wsgi', 'application')
+    os.remove(application)
+    file(application, 'w').write("""\
+#!/usr/bin/env python
+
+import os
+import sys
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+djangoapp = os.path.join(here, '..', 'libs')
+activate = os.path.join(here, '..', 'env', 'bin', 'activate_this.py')
+pythoneggs = os.path.join(here, '..', 'data', 'python-eggs')
+
+sys.path.append(djangoapp)
+execfile(activate, dict(__file__=activate))
+os.environ["PYTHON_EGG_CACHE"] = pythoneggs
+
+os.environ['DJANGO_SETTINGS_MODULE'] = '"""+app_name+""".settings'
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 """)
+    os.chmod(application, stat.S_IRWXU|stat.S_IRGRP|stat.S_IROTH)
 
-virtualenv.after_install = after_install
+def main():
+    parser = optparse.OptionParser(
+        usage = "%prog [OPTIONS] DEST_DIR")
 
-def adjust_options(options, args):
-    """
-    Force no site packages option and distribute option
-    """
-    options.no_site_packages = True
-    options.use_distribute = True
+    parser.add_option(
+        '-f', '--framework',
+        action = 'store',
+        dest = 'framework',
+        help = "Specifies used framework (flask | django)")
 
-virtualenv.adjust_options = adjust_options
+    parser.add_option(
+        '-n', '--name',
+        action = 'store',
+        dest = 'app_name',
+        help = "Name of your application folder ('application' is default)")
+
+    options, args = parser.parse_args()
+
+    if options.framework == None:
+        print "No framework specified!"
+        parser.print_help()
+        sys.exit(2)
+    elif options.framework.lower() not in ("flask", "django"):
+        print "Invalid framework specified!"
+        parser.print_help()
+        sys.exit(2)
+
+    if not args:
+        print "No DEST_DIR specified!"
+        parser.print_help()
+        sys.exit(2)
+    if len(args) > 1:
+        print "Invalid DEST_DIR given!"
+        parser.print_help()
+        sys.exit(2)
+
+    home_dir = args[0]
+
+    virtualenv.create_environment(home_dir, site_packages=False,
+                                  use_distribute=True)
+
+    after_install(home_dir, options)
 
 if __name__ == '__main__':
-    virtualenv.main()
+    main()
 
